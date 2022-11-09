3e3e8bf17a6252cae76b868f0b72042b4ae8cb12
Felipe Kessi <kessi@koders.cl>
2022-11-09 20:29:32 -0300

    Proyecto primer spring

---
+++ src/main/java/cl/infoclub/fsj/PrimerSpringApplication.java
@@ -0,0 +1,13 @@
+package cl.infoclub.fsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/infoclub/fsj/PrimerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.infoclub.fsj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
